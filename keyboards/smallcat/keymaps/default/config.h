e84d6ed01fc22aac4596ac0f6f5508841c865c48
Matthieu Petiteau <mpetiteau.pro@gmail.com>
2025-02-27 18:44:59 +0000

    Slight optimisations

--- keyboards/smallcat/keymaps/default/config.h
+++ keyboards/smallcat/keymaps/default/config.h
@@ -11,6 +11,9 @@
 #define MOUSEKEY_MAX_SPEED 4
 #define MOUSEKEY_TIME_TO_MAX 0
 #define SELECT_WORD_OS_MAC
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
+#define DISABLE_LEADER
 
 /* make use of the mounted WS2812 led from the Waveshare RP2040-zero */
 #define RGBLIGHT_LED_COUNT 1
--- keyboards/smallcat/keymaps/default/rules.mk
+++ keyboards/smallcat/keymaps/default/rules.mk
@@ -7,5 +7,10 @@ COMBO_ENABLE = yes
 VPATH += keyboards/gboards/
 SRC += features/select_word.c
 
+COMMAND_ENABLE = no
+UNICODE_ENABLE = no
+
 RGBLIGHT_ENABLE = yes
 WS2812_DRIVER = vendor
+
+EXTRAFLAGS += -flto
